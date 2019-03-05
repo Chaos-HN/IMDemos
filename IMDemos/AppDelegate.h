09aa4b3e4a34334a1504ab0451050f0fb5b2773d
oumeng <oumeng@oumengdeiMac.local>
2019-03-05 09:22:09 +0800

    Initial Commit

---
+++ IMDemos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IMDemos
+//
+//  Created by oumeng on 2019/3/5.
+//  Copyright © 2019 OYKM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
